850a6e2c5fed3de051092419744d39d47b04348f
Bart van Deenen <bart.vandeenen@kpn.com>
2017-11-10 13:56:52 +0100

    lowlevel works

---
+++ python-serserver/serportserver.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import socketserver
+import json
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        data = self.request.recv(1024).strip()
+        command = json.loads(data.decode('utf-8'))
+
+        print("{} wrote:".format(self.client_address[0]))
+        print(command)
+        # just send back the same data, but upper-cased
+        self.request.sendall("OK")
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
